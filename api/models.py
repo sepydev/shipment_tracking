799067439a86675d22060a6e7c5380a9c4b7fe2e
sepydev <mrprocs@gmail.com>
2023-09-01 08:11:18 +0330

    Create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Shipment(models.Model):
+    tracking_number = models.CharField(max_length=50)
+    carrier = models.CharField(max_length=50)
+    sender_address = models.CharField(max_length=500)
+    receiver_address = models.CharField(max_length=500)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.tracking_number} - {self.carrier}'
+
+
+class Article(models.Model):
+    shipment = models.ForeignKey(Shipment, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    SKU = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name} - {self.SKU}'
